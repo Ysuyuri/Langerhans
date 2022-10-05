8373a1789b7f3e8cd3f6dc9e5d3e5fedfb7788dd
Ysuyuri <108377373+Ysuyuri@users.noreply.github.com>
2022-10-04 22:45:45 -0300

    Jest Include

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,22 @@
 /**
  * @format
  */
-
+import { render } from '@testing-library/react'
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import 'react-dom'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import Login from '../src/pages/Login';
+import Exames from '../src/pages/exames';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+//it('renders correctly', () => {
+//  renderer.create(<Exames />);
+//});
+
+test ('sum', () => {
+  const { getByText } = render(<Exames />)
+  expect(getByText('Arquivos')).toBeTruthy()
+})
