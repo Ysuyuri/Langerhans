01f98e14132f9227c8cd5745826a81cf6d268189
Ysuyuri <108377373+Ysuyuri@users.noreply.github.com>
2022-10-06 13:13:01 -0300

    Revert "Jest Include"
    This reverts commit 8373a1789b7f3e8cd3f6dc9e5d3e5fedfb7788dd.

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import RealmContextProvider from './src/business/context/RealmContext';
 import AsyncStorage from '@react-native-community/async-storage';
-import 'react-native-gesture-handler'
 
 import Drawers from './src/routes/routes';
 import InitialRoute from './src/routes/routes';
